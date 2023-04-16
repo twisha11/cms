5d975a7c7c8693280a465a22cf8ccd41792e6eb2
Twisha <67854912+twisha11@users.noreply.github.com>
2023-04-16 20:44:48 +0530

    Update settings.py

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i4+l!^67!_kqmcp^9=mijgvs3!98@&-y+d0dgzu8#d=@9+k$#!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://vercel.com/']
 
 
 # Application definition
